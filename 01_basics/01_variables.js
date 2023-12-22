9212b2899038c2de156519cc4856662dd60018ba
Vedanshi19 <132483423+Vedanshi19@users.noreply.github.com>
2023-12-22 16:54:33 +0000

    let var and const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "12ka4@gmail.com"
+var accountPassword = "passhotum"
+accountCity = "udaipur"
+let accountState
+
+//accountId = 2 //not allowed
+accountEmail = "vp@vp.com"
+accountPassword = "7crore"
+accountCity = "delhi"
+
+// prefered not to use var because of issue in block scope and functional scope
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
