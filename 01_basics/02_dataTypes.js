6c4e439afb398299d997c2c06a7456b0f5d1991b
Vedanshi19 <132483423+Vedanshi19@users.noreply.github.com>
2023-12-23 09:43:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict" // treat all js code as newer version
+//alert(3+3) //we are using node js not browser
+//object
+console.log(typeof null); // null is a object
+console.log(typeof "vedanshi");
+console.log(typeof undefined); //type is undefined only
