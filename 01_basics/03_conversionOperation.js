de134510e36dc0310b49305a9328bce85e4ed073
Vedanshi19 <132483423+Vedanshi19@users.noreply.github.com>
2023-12-23 09:58:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = true
+
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //special type 
+
+/* "33" => 33
+   "33abc" => NaN
+   true=> 1 , false =>0
+   */
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+
+
+//"" => false
+//"ved" => true
